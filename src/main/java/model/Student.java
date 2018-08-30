ecd5ea84bebb08cb5386d2e0781335b46abdd173
Various Nabil <varionabilz@gmail.com>
2018-08-30 13:32:37 +0700

    first commit

---
+++ src/main/java/model/Student.java
@@ -0,0 +1,30 @@
+package model;
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean 
+public class Student {
+	private String firstName;
+	private String lastName;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}	
+	
+	@Override
+	public String toString() {
+		return "Student [firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
+	
+	public Student(){
+		
+	}
+}
